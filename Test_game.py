d853751f038e50f91eab522447d10b9fbf627fb2
Fadoua <fadelhannachi@gmail.com>
2022-07-26 20:02:04 +0200

    some questions added

--- Test_game.py
+++ Test_game.py
@@ -4,3 +4,22 @@ playing = input("Would you like to play? ")
 
 if playing != "yes" :
     quit()
+print ("Okay, let's play")
+
+answer1 = input("What is the capital of Macedonia? ").capitalize()
+if answer1 == "Skopje" :
+    print("You got it right! Congrats!")
+else : 
+    print("Wrong. The correct answer is Skopje")
+
+answer2 = input("What is the half of 6/2? ")
+if answer2 == "1,5" :
+    print("You didn't get tricked. Congrats!")
+else : 
+    print("Unfortunately, that is wrong. The correct answer was 1.5")
+
+answer3 = input("What is the most spoken language in the world in 2022? ").capitalize()
+if answer3 == "English" :
+    print("Exactly! With a total of 1.348 billion speakers, English is number one, not Mandarin Chinese!")
+else : 
+    print("Incorrect. The correct answer is English")
